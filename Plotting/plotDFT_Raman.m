dc99f11e51d6ee6bbee827ccdd82ffd9743afc3c
bradleyjamesryan <89863410+bradleyjamesryan@users.noreply.github.com>
2021-09-01 12:09:12 -0500

    Update plotDFT_Raman.m

--- Plotting/plotDFT_Raman.m
+++ Plotting/plotDFT_Raman.m
@@ -2,14 +2,12 @@
 % Data are fit to a Lorentzian distribution with a FWHM defined by the FWHM variable
 clear; clc;
 set(0,'defaultAxesFontName', 'Arial');
-axLineWidth = 1.5; plotLineWidth = 2; gridTransparency = .15; textSize = 16;
-LabelSize = 24; titleFontSize = 24; AxisNumberSize = 22; LegFontSize = 14;  zz = 1;
-FWHM = 30; x = [0:0.25:4000]; gamma = FWHM./2; count = 0;
+axLineWidth = 1.5; plotLineWidth = 2; LabelSize = 24; AxisNumberSize = 22; 
 
+FWHM = 30; x = [0:0.25:4000]; gamma = FWHM./2; count = 0;
 data = dlmread('path/to/vasp_raman.dat','',1,0);
 
-clf(figure(2)); hold on;
-
+clf(figure(1)); hold on; f = figure(1);  f.Position = [1 34 959 970];
 Lorentzian = zeros(1,length(x));
 for jj = 1:size(data,1)
     I = data(jj,5);        
@@ -28,7 +26,7 @@ set(findall(gca, 'Type', 'Line'),'LineWidth',plotLineWidth);
 set(gca,'linewidth',axLineWidth,'XMinorTick','on','YMinorTick','on','fontsize',AxisNumberSize,'tickdir','out','MinorGridLineStyle','-');
 xlabel('Raman Shift (cm^{-1})','FontSize', LabelSize, 'fontweight','b','color','k','FontName','Arial Bold');
 ylabel('Raman Scattering (Arb. Units)','FontSize', LabelSize, 'fontweight','b','color','k','FontName','Arial Bold');
-box on; grid off; ax = gca;ax.GridAlpha = gridTransparency;ax.XColor = 'k';ax.YColor = 'k';
+box on; grid off; ax = gca; ax.XColor = 'k';ax.YColor = 'k';
 
 
 
