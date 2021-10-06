77c99af3dca66e85aa641d5bf2e3babb3d4e836c
bradleyjamesryan <89863410+bradleyjamesryan@users.noreply.github.com>
2021-10-06 13:02:02 -0500

    Update driver_raman.sh

--- Raman_Calculation/Step3_Raman/driver_raman.sh
+++ Raman_Calculation/Step3_Raman/driver_raman.sh
@@ -3,14 +3,7 @@
 #SBATCH --nodes=1   # number of nodes, KEEP AT 1 ON EULER, unless running NEB jobs
 #SBATCH --ntasks-per-node=16   # 16 processor core(s) per node
 #SBATCH --mem=175G   # maximum memory per node
-#SBATCH --partition=long
 
 ulimit -s unlimited
 
-echo $HOSTNAME > a
-
-
-date >> a
 python ~/bin/vasp_raman.py > out_$(date +%m_%d_%Y_%H:%M:%S)
-date >> a
-
