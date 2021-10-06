c731f25c279f1eb235730f6f1d01d931522a36a7
bradleyjamesryan <89863410+bradleyjamesryan@users.noreply.github.com>
2021-10-06 13:07:00 -0500

    Create driver.sh

---
+++ FTIR_Calculation/Step1_Relax/driver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=0-00:20:00   # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes, KEEP AT 1 ON EULER, unless running NEB jobs
+#SBATCH --ntasks-per-node=16   # 16 processor core(s) per node
+#SBATCH --mem=175G   # maximum memory per node
+#SBATCH --partition=debug
+
+ulimit -s unlimited
+vasptype='vasp-ncl'
+outfile=out_$(date +%Y_%m_%d_%H:%M:%S)
+
+mpirun -n 16 /opt/bin/$vasptype > $outfile
