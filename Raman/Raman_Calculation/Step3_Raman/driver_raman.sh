4c76560236843a466c3e04833e9df103f1aa5306
bradleyjamesryan <89863410+bradleyjamesryan@users.noreply.github.com>
2021-10-06 12:48:05 -0500

    Delete Raman directory

--- Raman/Raman_Calculation/Step3_Raman/driver_raman.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=7-00:00:00   # walltime limit (HH:MM:SS)
-#SBATCH --nodes=1   # number of nodes, KEEP AT 1 ON EULER, unless running NEB jobs
-#SBATCH --ntasks-per-node=16   # 16 processor core(s) per node
-#SBATCH --mem=175G   # maximum memory per node
-
-ulimit -s unlimited
-
-python ~/bin/vasp_raman.py > out_$(date +%m_%d_%Y_%H:%M:%S)
